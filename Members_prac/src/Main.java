dcebd9d38d2a23699741867e3f92c879d07a4ceb
Shiva Kumar <nagalla.kumar@vedantu.com>
2023-07-27 14:06:57 +0530

    Added Members_prac

---
+++ Members_prac/src/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        MyClass c1 = new MyClass();
+        MySecondClass c2 = new MySecondClass();
+        c1.test();
+        c2.testFun();
+    }
+}
---
+++ Members_prac/src/TestClass.java
@@ -0,0 +1,13 @@
+class MyClass {
+    void test()
+    {
+        System.out.println("Hi From MyClass.test() function");
+    }
+}
+
+class MySecondClass {
+    void testFun()
+    {
+        System.out.println("Hi From MySecondClass.test() function");
+    }
+}
